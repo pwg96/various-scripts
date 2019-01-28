63b842c6b5c36abc96899072fff139ebbdc6cabe
pwg96 <pwg96pl@gmail.com>
2019-01-28 02:13:27 +0100

    OS detection and Windows support

--- logmedia.lua
+++ logmedia.lua
@@ -1,13 +1,20 @@
--- media history for mpv saved to ~/mpv.log
--- put it under ~/.config/mpv/scripts/
--- by pwg aka bodzioslaw (2016)
+--[[ playback history script for mpv by pwg96 aka bodzioslaw 
+  playback history will be available under your user directory eg. $HOME/mpv.log or $HOMEPATH\mpv.log 
+  put this file under ~/.config/mpv/scripts or %APPDATA%\mpv\scripts ]]
 
 function log_media()
-         f = io.open(os.getenv("HOME")..'/mpv.log', 'a')
-         fname = mp.get_property('filename')
-         stime = os.date(os.date('%d.%m.%Y %X'))
-         f:write(stime..' '..fname..'\n')
-         f:close()
+  dirsep = package.config:sub(1,1)
+
+  if dirsep == "/" then
+    f = io.open(os.getenv("HOME")..'/mpv.log', 'a')
+  elseif dirsep == "\\" then
+    f = io.open(os.getenv("USERPROFILE")..'\\mpv.log', 'a')
+  end
+
+  fname = mp.get_property('filename')
+  stime = os.date(os.date('%d.%m.%Y %X'))
+  f:write(stime..' '..fname..'\n')
+  f:close()
 end
  
 mp.register_event('file-loaded', log_media)
